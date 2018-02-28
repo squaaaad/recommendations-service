57134ead42e539e070ae4577c5d1b70524fa51f2
Stacy Lin <stayclin@gmail.com>
2018-02-28 10:51:42 -0800

    added restaurant dummy data to use state for photo carousel rendering. added styling for photo carousel buttons and functionality

--- client/src/components/RestaurantDetails.jsx
+++ client/src/components/RestaurantDetails.jsx
@@ -19,5 +19,4 @@ const RestaurantDetails = (props) => {
   )
 }
 
-
 export default RestaurantDetails;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM  from 'react-dom';
 import RestaurantCard from './components/RestaurantCard.jsx'
 
+const Restaurants = [
+  {name: 'Maven', photos: ['https://lh3.googleusercontent.com/p/AF1QipNR6KcJVNb3PKTpeli7dIZEOzFqs3ntcWug-U8Q=s1600-w400', 'https://lh3.ggpht.com/p/AF1QipNoMRZwGYU_wB9hp9AQXLlFL5Fx-phGRyX3kAg_=s224-c-v1-rj', 'https://lh3.ggpht.com/p/AF1QipNrd_l95_GAu4NjXMo8lvafHxAtvoHLjjXVbfxf=s224-c-v1-rj']},
+  {name: 'A', photos: ['https://lh3.googleusercontent.com/p/AF1QipNR6KcJVNb3PKTpeli7dIZEOzFqs3ntcWug-U8Q=s1600-w400', 'https://lh3.ggpht.com/p/AF1QipNoMRZwGYU_wB9hp9AQXLlFL5Fx-phGRyX3kAg_=s224-c-v1-rj']},
+  {name: 'B', photos: ['https://lh3.googleusercontent.com/p/AF1QipNR6KcJVNb3PKTpeli7dIZEOzFqs3ntcWug-U8Q=s1600-w400', 'https://lh3.ggpht.com/p/AF1QipNoMRZwGYU_wB9hp9AQXLlFL5Fx-phGRyX3kAg_=s224-c-v1-rj']},
+  {name: 'C', photos: ['https://lh3.googleusercontent.com/p/AF1QipNR6KcJVNb3PKTpeli7dIZEOzFqs3ntcWug-U8Q=s1600-w400', 'https://lh3.ggpht.com/p/AF1QipNoMRZwGYU_wB9hp9AQXLlFL5Fx-phGRyX3kAg_=s224-c-v1-rj']},
+  {name: 'D', photos: ['https://lh3.googleusercontent.com/p/AF1QipNR6KcJVNb3PKTpeli7dIZEOzFqs3ntcWug-U8Q=s1600-w400', 'https://lh3.ggpht.com/p/AF1QipNoMRZwGYU_wB9hp9AQXLlFL5Fx-phGRyX3kAg_=s224-c-v1-rj']},
+];
+
 class App extends React.Component{
   constructor(props){
     super(props);
@@ -11,12 +19,9 @@ class App extends React.Component{
 
     return(
       <div className="recommendations-container">
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
+        {Restaurants.map((restaurant, index) => (
+          <RestaurantCard restaurant={restaurant} key={index} />
+        ))}
       </div>
     )
   }
