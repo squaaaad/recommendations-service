6cef110916644848f93ed3bb48ce4f5e7479b242
Stacy Lin <stayclin@gmail.com>
2018-02-28 11:13:24 -0800

    changed RestaurantDetails to stateless component

--- client/src/components/RestaurantDetails.jsx
+++ client/src/components/RestaurantDetails.jsx
@@ -1,30 +1,23 @@
 import React from 'react';
 
-class RestaurantDetails extends React.Component {
-  constructor(props){
-    super(props);
-  }
-
-  render(){
-
-
-    return(
-      <div className="restaurant-details">
-        <div className="details-title">MAVEN</div>
-        <div className="details-tags">Californian · Lower Haight · $$</div>
-        <div className="details-ratings">
-          <img src="/zagat-logo-icon.png" className="zagat-rating-icon"></img><span className="zagat-rating">Food </span><span className="zagat-rating-value">4.3</span><div className="rating-divider"></div>
-          <img src="/google-logo-icon.png" className="google-rating-icon"></img><span className="google-rating-value">4.3</span>
-          <div className="google-rating-stars">
-            <div className="star-ratings-top" style={{width:'85%'}}><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
-            <div className="star-ratings-bottom"><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
-          </div>
-          <span className="google-rating-count">(68)</span>
+const RestaurantDetails = (props) => {
+  return(
+    <div className="restaurant-details">
+      <div className="details-title">MAVEN</div>
+      <div className="details-tags">Californian · Lower Haight · $$</div>
+      <div className="details-ratings">
+        <img src="/zagat-logo-icon.png" className="zagat-rating-icon"></img><span className="zagat-rating">Food </span><span className="zagat-rating-value">4.3</span><div className="rating-divider"></div>
+        <img src="/google-logo-icon.png" className="google-rating-icon"></img><span className="google-rating-value">4.3</span>
+        <div className="google-rating-stars">
+          <div className="star-ratings-top" style={{width:'85%'}}><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
+          <div className="star-ratings-bottom"><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
         </div>
-        <div className="details-description">Small plates and creative drinks in a modern setting with wall-mounted plants & polished log.</div>
+        <span className="google-rating-count">(68)</span>
       </div>
-    )
-  }
+      <div className="details-description">Small plates and creative drinks in a modern setting with wall-mounted plants & polished log.</div>
+    </div>
+  )
 }
+
 
 export default RestaurantDetails;
