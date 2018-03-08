eddfdd5e12ba0d41f3d9de7e2b65ec470caac07f
Stacy Lin <stayclin@gmail.com>
2018-03-07 17:31:14 -0800

    added css bundle to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -18,10 +18,10 @@ module.exports = {
           presets: ['react', 'es2015']
        }
       },
-      // {
-      //   test: /\.css$/,
-      //   use: ['style-loader','css-loader']
-      // }
+      {
+        test: /\.css$/,
+        use: ['style-loader','css-loader']
+      }
     ]
   }
 };
