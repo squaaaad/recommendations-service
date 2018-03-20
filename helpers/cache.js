9574aa2eee9dbc275567fe7121db7103731a6636
James Davidson <jfdavidson10@gmail.com>
2018-03-19 20:16:07 -0700

    code cleanup

--- helpers/cache.js
+++ helpers/cache.js
@@ -10,22 +10,16 @@ const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = async function () {
   const key = JSON.stringify(Object.assign({}, this.getQuery(), {collection: this.mongooseCollection.name}));
-  // See if we have a value for 'key' inside redis
   const cacheValue = await client.get(key);
 
-  // If we do return that; exec function expects a mongoose model
   if (cacheValue) {
-    //parse our cached value
     console.log('returned from cache');
     const doc = JSON.parse(cacheValue);
     return Array.isArray(doc)
-    // if it is an array make each object a model
       ? doc.map(d => new this.model(d))
-      // else its an object and just make it a model
       : new this.model(doc);
   }
 
-  // Otherwise, issue the query and store the result in redis
   const result = await exec.apply(this, arguments);
   client.set(key, JSON.stringify(result), 'EX', 86400);
   return result;
